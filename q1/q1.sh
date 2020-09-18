973e6c4d5ac8d19584da6d72a3b5bbdaa0366903
Akshar <akshars2801@gmail.com>
2020-09-18 21:20:43 +0530

    q1

---
+++ q1/q1.sh
@@ -0,0 +1,12 @@
+git init
+echo iitbhilai > 11940090.txt
+touch 11940840.txt 11940470.txt
+cp 11940090.txt 11940840.txt
+cp 11940090.txt 11940470.txt
+mkdir Akshar Harshil Porash
+mv 11940090.txt Akshar
+mv 11940470.txt Harshil
+mv 11940840.txt Porash
+git add .
+git commit -m "All at once"
+git graph
