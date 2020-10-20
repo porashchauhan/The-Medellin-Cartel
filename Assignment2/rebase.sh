9eb7e08b54f68529ed5f3d5648fee1ce898c0290
akshar <akshars2801@gmail.com>
2020-10-20 22:24:26 +0530

    added 3a.sh and 3b.sh

---
+++ Assignment2/rebase.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+git init
+touch a.txt
+echo "hello" > a.txt
+git add a.txt
+git commit -m"commit m1"
+git branch feature
+git checkout feature
+touch b.txt
+echo "hi" > b.txt
+git add b.txt
+git commit -m"commit f1"
+echo"hi1" > b.txt
+git add b.txt
+git commit -m"commit f2"
+git checkout master
+echo "hello1" > a.txt
+git add a.txt
+git commit -m "m2"
+echo "hello2" > a.txt
+git add a.txt
+git commit -m "m3"
+git graph 
+sleep 1
+git rebase feature 
+git graph
